d1c40047a16180d08368cba4e240a455e150bfc1
Dave <48577149+dfuent@users.noreply.github.com>
2020-12-08 21:59:18 -0500

    Small changes

--- data_cleansing_final.R
+++ data_cleansing_final.R
@@ -1,6 +1,6 @@
 #STAT 6021 Project 2 (Hate Crimes in America)
 #David Fuentes, Matt Villhauer, Matt Nicklas
-#Using the data set from FiveThirtyEight to see which variables contribute to hate crimes measure as rate per 100K residents 
+#Using data from FiveThirtyEight, RAND Corp, and other sources to see which variables contribute to hate crimes measured as rate per 100K residents 
 
 # #########################################################################
 # DATA CLEANSING
@@ -8,7 +8,7 @@
 
 library(dplyr)
 
-#Read in csv files we will use to combine into one large csv
+#Read in csv files we will combine into one large csv
 elast_df <- read.csv('elasticity.csv', header = TRUE, fileEncoding='UTF-8-BOM')
 hc_df <- read.csv('hatecrimes.csv', header = TRUE, fileEncoding='UTF-8-BOM')
 police_df <- read.csv('police_killings.csv', header = TRUE, fileEncoding='UTF-8-BOM')
@@ -16,9 +16,11 @@ state_map <- read.csv('mapping.csv', header = TRUE, fileEncoding='UTF-8-BOM')
 gun_df <- read.csv('RAND_gun_ownership.csv', header = TRUE, fileEncoding='UTF-8-BOM')
 fbi_2019 <- read.csv('2019_fbi.csv', header = TRUE, fileEncoding='UTF-8-BOM')
 
+#Count the number of citizen deaths at the hands of the police
 pol_count <- police_df %>% count(state)
 pol_count
 
+#Filter RAND data for 2016
 gun_df <- gun_df[gun_df$Year == 2016,]
 gun_df
 
@@ -26,7 +28,7 @@ elast_df[1,] # state = abbrev
 hc_df[1,] # state = full
 police_df[1,] # state = abbrev
 
-#Rename the state names to be the same that way you can join using the states 
+#Standardize the state-name columns to be the same so join will work 
 elast_df<-rename(elast_df, state_abbrev=state)
 hc_df<-rename(hc_df, state_full=state)
 police_df<-rename(police_df, state_abbrev=state)
@@ -34,9 +36,10 @@ gun_df<-rename(gun_df, state_full=STATE)
 pol_count<-rename(pol_count, state_abbrev=state)
 pol_count<-rename(pol_count, pk_count=n)
 
-colnames(elast_df) # state = abbrev
-colnames(hc_df) # state = full
-colnames(police_df) # state = abbrev
+#Check columns
+colnames(elast_df) 
+colnames(hc_df) 
+colnames(police_df) 
 colnames(state_map)
 colnames(pol_count)
 
@@ -47,6 +50,7 @@ hc_df<-left_join(hc_df, gun_df, by = 'state_full')
 hc_df<-left_join(hc_df, pol_count, by = 'state_abbrev')
 hc_df<-left_join(hc_df, fbi_2019, by = 'state_full')
 
+#Create police killing DF
 police_df<-left_join(police_df, state_map, by = 'state_abbrev')
 
 elast_df[1,] # state = abbrev
@@ -55,10 +59,11 @@ police_df[1,] # state = abbrev
 
 police_df
 
+#Join all data
 df_full<-left_join(police_df, hc_df, by = 'state_full')
 df_full
 
-#Remove the extra columns with double the information confederate and state_abbrev come from multiple dataframes
+#Remove the extra columns with redundant information, e.g. confederate and state_abbrev, which come from multiple dataframes
 df_full<-df_full[ , !(names(df_full) %in% c('state_abbrev.x', 'confederate.x', 'confederate.y', 'state_abbrev.y'))]
 
 # #########################################################################
@@ -66,6 +71,7 @@ df_full<-df_full[ , !(names(df_full) %in% c('state_abbrev.x', 'confederate.x', '
 hc_df[1,] # state = full
 hc_df<-hc_df[!apply(hc_df['state_full'] == "", 1, all),]
 
+#Factor and add levels to binary data
 is.factor(hc_df$confederate)
 hc_df$confederate <- factor(hc_df$confederate, levels = c(0, 1), labels = c('No', 'Yes'))
 
@@ -75,18 +81,20 @@ hc_df$universl <- factor(hc_df$universl, levels = c(0, 1), labels = c('No', 'Yes
 is.factor(hc_df$permit)
 hc_df$permit <- factor(hc_df$permit, levels = c(0, 1), labels = c('No', 'Yes'))
 
+#calc police killings per capita
 hc_df$pk_count[is.na(hc_df$pk_count)] <- 0
 hc_df$pk_percap <- hc_df$pk_count / hc_df$population
 
 #hc_df$incidents <- as.numeric(hc_df$incidents)
 #hc_df$pop_covered <- as.numeric(hc_df$pop_covered)
 
+#add 2019 FBI hate-crime data
 hc_df$fbi_2019_per100k <- hc_df$incidents/hc_df$pop_covered*100000
 
 hc_full_df<-hc_df
 
 # #########################################################################
-# This section adds in another two csv files and joins to the large dataframe above 
+# This section adds in another two csv files containing hate-group counts per state for 2016 and 2019 and joins to the large dataframe above 
 
 # Add in the 2016 Data
 hate_group_2016_df <- read.csv('hate_group_count_2016.csv', header = TRUE, fileEncoding='UTF-8-BOM')
@@ -109,7 +117,7 @@ hc_full_df<-left_join(hc_full_df, hate_group_2019_df, by = 'state_full')
 
 hc_full_df$hate_group_count_2019[is.na(hc_full_df$hate_group_count_2019)] <- 0
 
-#Creates a column that tells you wether a state was confederate or has a background check for guns or has neither
+#Creates a column that tells you wether a state was confederate, has a background check for guns, is neither, or is both
 hc_full_df<- transform(hc_full_df, con_uni_combo=ifelse(universl == 'No' & confederate == 'No',
                                                         'Neither', 
                                                         ifelse(universl == 'No' & confederate == 'Yes', 
